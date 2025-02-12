52860a15bfbf2085f2e63a421ed526b3a859a981
Catalin Nichita <nichitaandrei21@gmail.com>
2025-02-12 10:52:20 +0200

    fixed sdk log errors

--- taf/src/test/java/tests/stepdefinitions/StepDefinitions.java
+++ taf/src/test/java/tests/stepdefinitions/StepDefinitions.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import com.example.testautomationframework.implementation.EmagHomePageImpl;
